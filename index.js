ef1ca73eea109c20646600d5f00360bdeac8de56
PunithMohan <87635208+PunithMohan@users.noreply.github.com>
2021-11-23 22:57:07 +0530

    time

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ try {
   // `who-to-greet` input defined in action metadata file
   const commitCount = core.getInput('commit-count');
   console.log(`Commit Count ${commitCount}!`);
-  //const time = (new Date()).toTimeString();
- // core.setOutput("time", time);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
