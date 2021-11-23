609c45760640f11373ea37163c97828a075b31b0
YS <ys@email.com>
2021-11-24 00:24:06 +0530

    changes added for tag

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ try {
   console.log(`Commit Count ${commitCount}!`);
   var CommitsCounts = commitCount;
   core.setOutput("CommitsCounts", CommitsCounts);
+  const labelName = core.getInput('LabelName');
+  var labelname = labelName;
+  core.setOutput("labelname", labelname);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
