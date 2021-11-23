d4a64e22380a7085e69b8400060430b963baafb3
YS <ys@email.com>
2021-11-23 23:38:44 +0530

    changes added for tag

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ try {
   // `who-to-greet` input defined in action metadata file
   const commitCount = core.getInput('commit-count');
   console.log(`Commit Count ${commitCount}!`);
-  const commitsCounts = core.getInput('Commits-Counts');
-  core.setOutput("commitsCounts", 4);
+  const commitsCounts = 4;
+  core.setOutput("commitsCounts", commitsCounts);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
